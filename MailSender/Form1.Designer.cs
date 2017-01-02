010bab89658f6a290b4369136a82c5be19ea5e1b
andyNg <tynshjt@gmail.com>
2017-01-02 18:33:35 +0200

    With MySQL localhost

---
+++ MailSender/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace MailSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.subject = new System.Windows.Forms.TextBox();
+            this.body = new System.Windows.Forms.TextBox();
+            this.cbFrom = new System.Windows.Forms.ComboBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.cbto = new System.Windows.Forms.ComboBox();
+            this.Dear = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Subject";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Dear";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // subject
+            // 
+            this.subject.Location = new System.Drawing.Point(114, 95);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(157, 20);
+            this.subject.TabIndex = 12;
+            // 
+            // body
+            // 
+            this.body.Location = new System.Drawing.Point(114, 163);
+            this.body.Multiline = true;
+            this.body.Name = "body";
+            this.body.Size = new System.Drawing.Size(212, 72);
+            this.body.TabIndex = 13;
+            // 
+            // cbFrom
+            // 
+            this.cbFrom.FormattingEnabled = true;
+            this.cbFrom.Items.AddRange(new object[] {
+            "tynshjt@gmail.com",
+            "ndng96@gmail.com",
+            "duongnguyen151196@gmail.com",
+            "nduong96@outlook.com"});
+            this.cbFrom.Location = new System.Drawing.Point(114, 25);
+            this.cbFrom.Name = "cbFrom";
+            this.cbFrom.Size = new System.Drawing.Size(157, 21);
+            this.cbFrom.TabIndex = 14;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(114, 252);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(76, 33);
+            this.btnSend.TabIndex = 16;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // cbto
+            // 
+            this.cbto.FormattingEnabled = true;
+            this.cbto.Items.AddRange(new object[] {
+            "tynshjt@gmail.com",
+            "ndng96@gmail.com",
+            "duongnguyen151196@gmail.com",
+            "nduong96@outlook.com"});
+            this.cbto.Location = new System.Drawing.Point(114, 59);
+            this.cbto.Name = "cbto";
+            this.cbto.Size = new System.Drawing.Size(157, 21);
+            this.cbto.TabIndex = 17;
+            // 
+            // Dear
+            // 
+            this.Dear.Location = new System.Drawing.Point(114, 137);
+            this.Dear.Name = "Dear";
+            this.Dear.Size = new System.Drawing.Size(157, 20);
+            this.Dear.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Message";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 319);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(350, 22);
+            this.statusStrip1.TabIndex = 20;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(152, 17);
+            this.toolStripStatusLabel1.Text = "Connect to localhost to use";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 341);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Dear);
+            this.Controls.Add(this.cbto);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.cbFrom);
+            this.Controls.Add(this.body);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Email Sender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox subject;
+        private System.Windows.Forms.TextBox body;
+        private System.Windows.Forms.ComboBox cbFrom;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ComboBox cbto;
+        private System.Windows.Forms.TextBox Dear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ MailSender/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using MySql.Data.MySqlClient;
+
+namespace MailSender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            cbFrom.SelectedIndex = 1;
+            body.Font = new Font(body.Font, FontStyle.Italic);
+        }
+        static string pass, server,signature,head,content;
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+
+        } // nothing
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            content = "";           
+            getPassWordandServer(); //generate server and password
+            // head of message ex: "Dear Maria"
+            head = "<h3>Dear " +Dear.Text + ",</h3>";
+            signature = "<address><ul><li>Duong Nguyen </li><li>Student at Vaasa University of Applied Sciences</li><li>www.cc.puv.fi/~e1500948</li><ul></address>";
+            #region try send mail
+            try
+            {
+            MailMessage mail = new MailMessage();
+            SmtpClient Client = new SmtpClient(server,587);
+            mail.From = new MailAddress(cbFrom.Text);
+            mail.To.Add(cbto.Text);
+            mail.Subject = subject.Text;
+            // Seperate body line in line
+            foreach (string line in body.Lines)
+            {
+                if (line != "")
+                {
+                    content += "<h3>" + line.Trim() + "</h3>";
+                }
+            }
+            mail.Body =head + content.TrimEnd() + signature;
+            mail.IsBodyHtml = true;
+            
+            
+            Client.EnableSsl = true;
+            Client.Credentials = new System.Net.NetworkCredential(cbFrom.Text, pass);
+            
+                Client.Send(mail);
+                MessageBox.Show("Email sent!","Success");
+            }
+            #endregion try
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        //  get password and server for SMTP
+        private void getPassWordandServer()
+        {
+            int id = (cbFrom.SelectedIndex);
+            String connString = "Server=localhost;database=e1500948_email;uid=root;pwd=;";
+            MySqlConnection conn = new MySqlConnection(connString);
+            MySqlCommand command;
+            command = conn.CreateCommand();
+            switch (id)
+            {
+                case 0:
+                    command.CommandText = "SELECT * FROM `emailSMTP` WHERE `id`='1'";
+                    break;
+                case 1:
+                    command.CommandText = "SELECT * FROM `emailSMTP` WHERE `id`='2'";
+                    break;
+                case 3:
+                    command.CommandText = "SELECT * FROM `emailSMTP` WHERE `id`='4'";
+                
+                    break;
+            }                   
+            try
+            {
+                conn.Open();
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    pass = reader["password"].ToString();
+                    server = reader["server"].ToString();
+                }
+            }
+          //  catch (MySql.Data.MySqlClient.MySqlException ex )
+          catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+            finally
+            {
+                conn.Close(); //close connection
+                conn.Dispose(); // free resource
+                conn = null;    //cancle object, empty object
+                //MessageBox.Show("Close connection");
+            }
+            
+        } // end of getPassword
+
+        
+
+    }
+}
